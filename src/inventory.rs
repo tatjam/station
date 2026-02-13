4bc89e961ec16b4ba2c974571d282a471dfb3f9d
tatjam <tatjam02@gmail.com>
2026-02-14 00:33:45 +0100

    Lots of progress on the query of the inventory, nearly done

--- src/inventory.rs
+++ src/inventory.rs
@@ -7,11 +7,12 @@ use axum::{
 };
 use maud::html;
 use serde::Deserialize;
-use sqlx::QueryBuilder;
+use sqlx::{Postgres, QueryBuilder, pool::PoolConnection};
 use tracing::{error, info};
 
 const ALL_CATEGORIES_STR: &'static str = "All Categories";
 const ALL_FOOTPRINTS_STR: &'static str = "All Footprints";
+const NO_FOOTPRINT_STR: &'static str = "No Footprint";
 
 #[derive(Debug, Deserialize)]
 pub struct SearchForm {
@@ -25,14 +26,14 @@ pub struct SearchForm {
 
 #[derive(Debug, sqlx::FromRow)]
 pub struct InventoryItem {
-    id: i32,
-    mpn: String,
+    id: i64,
+    mpn: Option<String>,
     category: String,
-    footprint: String,
-    value: f64,
-    location: String,
-    quantity: i32,
-    comment: String,
+    footprint: Option<String>,
+    value: Option<f32>,
+    location: Option<String>,
+    quantity: i64,
+    comments: Option<String>,
 }
 
 use crate::state::AppState;
@@ -49,19 +50,10 @@ pub fn handle_generic_inventory_error<E: Display>(e: E) -> Html<String> {
     );
 }
 
-pub async fn search_handler(
-    State(state): State<AppState>,
-    Form(search): Form<SearchForm>,
-) -> impl IntoResponse {
-    info!("Performing search query: {:?}", search);
-
-    let mut db_conn = match state.pool.acquire().await {
-        Ok(conn) => conn,
-        Err(e) => {
-            return handle_generic_inventory_error(e);
-        }
-    };
-
+async fn query_inventory(
+    search: &SearchForm,
+    db_conn: &mut PoolConnection<Postgres>,
+) -> Result<Vec<InventoryItem>, sqlx::Error> {
     let mut query = QueryBuilder::new("SELECT * FROM inventory WHERE 1=1");
     if search.category != ALL_CATEGORIES_STR && !search.category.is_empty() {
         query.push(" AND category = ");
@@ -69,8 +61,12 @@ pub async fn search_handler(
     }
 
     if search.footprint != ALL_FOOTPRINTS_STR && !search.footprint.is_empty() {
-        query.push(" AND footprint = ");
-        query.push_bind(&search.category);
+        if search.footprint == NO_FOOTPRINT_STR {
+            query.push(" AND footprint IS NULL");
+        } else {
+            query.push(" AND footprint = ");
+            query.push_bind(&search.footprint);
+        }
     }
 
     if search.in_stock == "on" {
@@ -96,7 +92,7 @@ pub async fn search_handler(
         query.push_bind(format!("%{}%", search.search));
         query.push(" OR category ILIKE ");
         query.push_bind(format!("%{}%", search.search));
-        query.push(" OR comment ILIKE ");
+        query.push(" OR comments ILIKE ");
         query.push_bind(format!("%{}%", search.search));
         query.push(")");
     }
@@ -107,14 +103,129 @@ pub async fn search_handler(
 
     info!("Database query: {}", sql);
 
-    let results = match query
+    query
         .build_query_as::<InventoryItem>()
-        .fetch_all(&mut *db_conn)
+        .fetch_all(db_conn.as_mut())
         .await
-    {
-        Ok(items) => items,
-        Err(e) => return handle_generic_inventory_error(e),
+}
+
+fn format_mult_value(value: f32) -> String {
+    if value < 1e-9 {
+        format!("{:.2} p", value * 1e12)
+    } else if value < 1e-6 {
+        format!("{:.2} n", value * 1e9)
+    } else if value < 1e-3 {
+        format!("{:.2} µ", value * 1e6)
+    } else if value < 1e0 {
+        format!("{:.2} m", value * 1e3)
+    } else if value < 1e3 {
+        format!("{:.2}  ", value * 1e0)
+    } else if value < 1e6 {
+        format!("{:.2} k", value * 1e-3)
+    } else if value < 1e9 {
+        format!("{:.2} M", value * 1e-6)
+    } else {
+        format!("{:.2} G", value * 1e-9)
+    }
+}
+fn format_value(category: &String, value: f32) -> String {
+    let (unit, mult) = match category.as_str() {
+        "CapCeramic" => ("F", true),
+        "CapElectro" => ("F", true),
+        "Resistor" => ("Ω", true),
+        "Inductor" => ("H", true),
+        _ => ("", false),
+    };
+    let value = if mult {
+        format_mult_value(value)
+    } else {
+        format!("{:.2}  ", value)
     };
 
-    Html(format!("Hi"))
+    format!("{}{}", value, unit)
+}
+
+pub async fn search_handler(
+    State(state): State<AppState>,
+    Form(search): Form<SearchForm>,
+) -> impl IntoResponse {
+    info!("Performing search query: {:?}", search);
+
+    let mut db_conn = match state.pool.acquire().await {
+        Ok(conn) => conn,
+        Err(e) => {
+            return handle_generic_inventory_error(e);
+        }
+    };
+
+    let results = match query_inventory(&search, &mut db_conn).await {
+        Ok(results) => results,
+        Err(e) => {
+            return handle_generic_inventory_error(e);
+        }
+    };
+
+    let response = html! {
+        table class="striped" {
+            thead {
+                tr {
+                    th id="sort-mpn" onclick="sortBy('mpn')" style="cursor: pointer; font-weight: normal" scope="col" {
+                        "MPN"
+                    }
+                    th id="sort-category" onclick="sortBy('category')" style="cursor: pointer; font-weight: normal" scope="col" {
+                        "Category"
+                    }
+                    th id="sort-footprint" onclick="sortBy('footprint')" style="cursor: pointer; font-weight: normal" scope="col" {
+                        "Footprint"
+                    }
+                    th id="sort-value" onclick="sortBy('value')" style="cursor: pointer; font-weight: normal" scope="col" {
+                        "Value"
+                    }
+                    th id="sort-quantity" onclick="sortBy('quantity')" style="cursor: pointer; font-weight: normal" scope="col" {
+                        "Qty."
+                    }
+                    th {
+                        "Action"
+                    }
+                }
+            }
+            @for result in &results {
+                tr {
+                    th scope="row" {
+                        @if let Some(mpn) = &result.mpn {
+                            (mpn)
+                        } @else {
+                            "—"
+                        }
+                    }
+                    td {
+                        (result.category)
+                    }
+                    td {
+                        @if let Some(footprint) = &result.footprint {
+                            (footprint)
+                        } @else {
+                            "—"
+                        }
+                    }
+                    td style="text-align: right; font-family: monospace; font-size: 1.3em; white-space: pre; width: 1%" {
+                        @if let Some(value) = result.value {
+                            (format_value(&result.category, value))
+                        } @else {
+                            "—"
+                        }
+                    }
+                    td {
+                        (result.quantity)
+                    }
+                    td {
+                        "TODO"
+                    }
+                }
+            }
+        }
+    }
+    .into_string();
+
+    Html(response)
 }
