4798b9d57de5f4f10a7407fbf12a4b383c63506d
tatjam <tatjam02@gmail.com>
2026-02-14 13:28:27 +0100

    Switch to musl libc, maintenance scripts

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-cargo build --release
-scp ./target/release/station tatjam@ssh-tatjam.alwaysdata.net:/home/tatjam/station/station
+cargo build --release --target x86_64-unknown-linux-musl
+scp target/x86_64-unknown-linux-musl/release/station tatjam@ssh-tatjam.alwaysdata.net:/home/tatjam/station/station
---
+++ load_backup.sh
@@ -0,0 +1,3 @@
+source .env 
+export PGPASSWORD=${DB_PASSWORD}
+psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} < $1
